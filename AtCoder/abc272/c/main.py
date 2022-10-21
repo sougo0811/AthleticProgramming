28f44b832d4ef87105074aa77feb59995981aaeb
sougo0811 <sougo2813@outlook.jp>
2022-10-21 18:04:54 +0900

    修正

--- AtCoder/abc272/c/main.py
+++ AtCoder/abc272/c/main.py
@@ -4,22 +4,22 @@ input = sys.stdin.readline
 N = int(input())
 AN = list(map(int,input().split()))
 
-odd_count = 0; even_cnt = 0; max_1 = 0; max_2 = 0; max_3 = 0; max_4 = 0
+odd_count = 0; even_cnt = 0; even_max_1 = 0; even_max_2 = 0; odd_max_1 = 0; odd_max_2 = 0
 for i in range(N):
   if AN[i]%2 == 0:
     even_cnt += 1
-    max_2 = max(max_2,AN[i])
-    if max_1 < max_2:
-      max_1,max_2 = max_2,max_1
+    even_max_2 = max(even_max_2,AN[i])
+    if even_max_1 < even_max_2:
+      even_max_1,even_max_2 = even_max_2,even_max_1
   else:
     odd_count += 1
-    max_4 = max(max_4,AN[i])
-    if max_3 < max_4:
-      max_3,max_4 = max_4,max_3
+    odd_max_2 = max(odd_max_2,AN[i])
+    if odd_max_1 < odd_max_2:
+      odd_max_1,odd_max_2 = odd_max_2,odd_max_1
 if even_cnt < 2 and odd_count < 2:
   print(-1)
 else:
-  if max_1 + max_2 >= max_3 + max_4:
-    print(max_1 + max_2)
+  if even_max_1 + even_max_2 >= odd_max_1 + odd_max_2:
+    print(even_max_1 + even_max_2)
   else:
-    print(max_3 + max_4)
+    print(odd_max_1 + odd_max_2)
