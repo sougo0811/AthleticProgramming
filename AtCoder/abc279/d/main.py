f52ec055748673268a7a6d5f9fd01f1f5a2e7e33
sougo0811 <sougo2813@outlook.jp>
2022-12-03 00:07:46 +0900

    solved

--- AtCoder/abc279/d/main.py
+++ AtCoder/abc279/d/main.py
@@ -2,17 +2,13 @@ import sys
 input = sys.stdin.readline
 
 A,B = map(int,input().split())
-
-
-min_x = ((A/2*B)**(2/3))-1
-
+min_x = ((A/(2*B))**(2/3))-1
 def f(n):
-  global A,B
   return B*n + A/((n+1)**(1/2))
 
 if min_x >= (A/B):
   print(f(0))
-elif 0 <= min_x < (A/B):
+else:
   ans_li = [i+int(min_x) for i in range(-5,5) if i+int(min_x) >= 0]
   ans = f(int(min_x))
   for i in range(len(ans_li)):
