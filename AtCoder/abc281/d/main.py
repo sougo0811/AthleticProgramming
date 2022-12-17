2d97c1e4d4eb55d34b469988113e845d1538fa74
sougo0811 <sougo2813@outlook.jp>
2022-12-17 18:03:11 +0900

    2022/12/16

--- AtCoder/abc281/d/main.py
+++ AtCoder/abc281/d/main.py
@@ -6,7 +6,12 @@ import itertools
 N,K,D = map(int,input().split())
 A = list(map(int,input().split()))
 
-#TEL
+#DP(動的計画法)
+dp = []
+
+
+'''
+#TEL 愚直
 Sa = list(itertools.combinations(A,K))
 Sb = [sum(i) for i in Sa if sum(i)%D == 0]
 S = set(Sb)
@@ -14,3 +19,4 @@ if len(S) == 0:
   print(-1)
 else:
   print(max(S))
+'''
---
+++ アルゴリズムと数学/028/main.py
@@ -0,0 +1,8 @@
+N = int(input())
+h = list(map(int,input().split()))
+
+dp = [0 for _ in range(N)]
+dp[1] = abs(h[0] - h[1])
+for i in range(2,N):
+  dp[i] = min(dp[i-1]+abs(h[i]-h[i-1]),dp[i-2]+abs(h[i]-h[i-2]))
+print(dp[-1])
